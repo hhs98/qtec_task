254d10976d85ac2f73ecdc960e8521033cfe2e3d
Hasoibul Hasan Siju <h.h.siju97@gmail.com>
2020-11-03 12:49:07 +0600

    whitenoise fix

--- task/settings.py
+++ task/settings.py
@@ -32,6 +32,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
